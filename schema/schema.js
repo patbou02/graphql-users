228c4b32ee75939c4905b09569a0d4f11b271d2f
Patrick Boutin <patrick.boutin@roberthalf.com>
2023-03-22 01:23:08 -0700

    Installed external package Axios for fetching data. In schema file, removed dummy data and lodash library and replaced contents of resolve method with axios get() method pointing at server endpoint for getting users based on ID.

--- schema/schema.js
+++ schema/schema.js
@@ -1,16 +1,11 @@
 const graphql = require('graphql');
 const { GraphQLString, GraphQLInt } = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 
 const {
   GraphQLObjectType,
   GraphQLSchema
 } = graphql;
-
-const users = [
-  { id: '23', firstName: 'Jason', age: 20 },
-  { id: '46', firstName: 'Alexis', age: 54 },
-];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -28,7 +23,8 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(resp => resp.data);
       },
     }
   }
